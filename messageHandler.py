58d76d55670d3e249108dd57c108dd740159615a
Xofanne <conygioso@gmail.com>
2025-05-21 16:48:45 -0300

    adjusted so links are transformed to 'link <site's name>' instead of readind the whole link

--- messageHandler.py
+++ messageHandler.py
@@ -9,16 +9,28 @@ import re
 from random import randint
 import ttsFile
 
+
+
+
 # regex simples para verificar se a pessoa chamou uma rolagem de dados
 # ex:   "3d20" é uma chamada válida
 #       "3d20 qualquer coisa depois na mensagem" não é uma chamada válida
 roll_20_re = "^[0-9]+[d][0-9]+$"
+
+# regex para verificar se o texto é um site
+linkhttps = "https://"
+linkhttp = "http://"
+
 
 # toda mensagem deve passar por esse messageHandler e a partir daqui será definido
 # qual a tratativa da mensagem
 async def messageHandler(msg, bot):
         if re.search(roll_20_re, msg.content):
                 await roll20(msg, bot)
+        elif re.search(linkhttps, msg.content):
+                await linkMsgHttps(msg, bot)
+        elif re.search(linkhttp, msg.content):
+                await linkMsgHttp(msg, bot)
         else:
                 await ttsFile.buildTTS(tts_query=msg, bot=bot)
 
@@ -34,4 +46,20 @@ async def roll20(msg, bot):
                 texto += f"dado nº {tries+1} -> {atual}\n"
         msg.content = f"{msg.author.display_name} rolou {rolls[0]} d {rolls[1]} e tirou {total}"
         await msg.channel.send(texto + msg.content)
+        await ttsFile.buildTTS(tts_query=msg, bot=bot)
+
+async def linkMsgHttps(msg, bot):
+        site = msg.content.replace("https://", ".https://.").split(".")
+        if site[site.index("https://")+1] == "www":
+                msg.content = msg.content.replace(msg.content[msg.content.find("https://"):msg.content.find(" ", msg.content.find("https://"))+1], f"link {site[site.index("https://")+2]} ")
+        else:
+                msg.content = msg.content.replace(msg.content[msg.content.find("https://"):msg.content.find(" ", msg.content.find("https://"))+1], f"link {site[site.index("https://")+1]} ")
+        await ttsFile.buildTTS(tts_query=msg, bot=bot)
+
+async def linkMsgHttp(msg, bot):
+        site = msg.content.replace("http://", ".http://.").split(".")
+        if site[site.index("http://")+1] == "www":
+                msg.content = msg.content.replace(msg.content[msg.content.find("http://"):msg.content.find(" ", msg.content.find("http://"))+1], f"link {site[site.index("http://")+2]} ")
+        else:
+                msg.content = msg.content.replace(msg.content[msg.content.find("http://"):msg.content.find(" ", msg.content.find("http://"))+1], f"link {site[site.index("http://")+1]} ")
         await ttsFile.buildTTS(tts_query=msg, bot=bot)
